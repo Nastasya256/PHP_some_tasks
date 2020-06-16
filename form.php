b24b2a988298363ae607fd8e6a723a4aeb8c302d
Nastasya Zhivaeva <nastasyazzhh@gmail.com>
2020-06-16 11:13:12 +0300

    Upload form

---
+++ form.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form class="" action="upload.php" method="POST" enctype="multipart/form-data">
+      <input type="file" name="file" value="">
+      <button type="submit" name="submit">UPLOAD</button>
+
+    </form>
+  </body>
+</html>
